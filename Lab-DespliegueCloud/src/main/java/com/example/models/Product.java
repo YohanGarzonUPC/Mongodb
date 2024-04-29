8875f509bbbe728bb5bebe3d50018e8503caf1dd
YohanGarzonUPC <111005416+YohanGarzonUPC@users.noreply.github.com>
2024-04-28 21:19:56 -0500

    holii
    holii

---
+++ Lab-DespliegueCloud/src/main/java/com/example/models/Product.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.models;
+
+import com.sun.istack.NotNull;
+import java.util.Calendar;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.eclipse.persistence.nosql.annotations.DataFormatType;
+import org.eclipse.persistence.nosql.annotations.NoSql;
+
+/**
+ *
+ * @author User
+ */
+@NoSql(dataFormat = DataFormatType.MAPPED)
+@Embeddable
+@XmlRootElement
+public class Product {
+
+    private String nombre;
+    private String marca;
+    private String fechacompra;
+
+    public String getFechacompra() {
+        return fechacompra;
+    }
+
+    public void setFechacompra(String fechacompra) {
+        this.fechacompra = fechacompra;
+    }
+
+    public Product() {
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+}
